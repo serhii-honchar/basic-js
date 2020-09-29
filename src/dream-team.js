6ccebcc827662848386cf76a4e8a38a38776f6d8
sergyi.gonchar <sergyi.gonchar@gmail.com>
2020-09-29 03:10:40 +0300

    feat: resolve dream-team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,20 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function createDreamTeam(members) {
+  if(members===null || members === undefined) {
+    return false;
+  }
+
+  let firstLetters = '';
+  for (let i = 0; i<members.length; i++){
+      let arg = members[i];
+      if(typeof(arg) === 'string' && arg.trim()!==''){
+        firstLetters+=arg.trim().slice(0,1).toLocaleUpperCase();
+      }
+  }
+
+  return firstLetters
+      .split('')
+      .sort()
+      .join('');
+}
