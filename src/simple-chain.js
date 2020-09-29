8d9f34b09e52648eba3ba3d00b65216b98cae3c1
sergyi.gonchar <sergyi.gonchar@gmail.com>
2020-09-29 23:56:39 +0300

    feat: resolve simple-chain

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,26 +1,49 @@
 const CustomError = require("../extensions/custom-error");
 
 const chainMaker = {
-  getLength() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  },
-  addLink(value) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  },
-  removeLink(position) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  },
-  reverseChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  },
-  finishChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }
+    getLength() {
+        return this.chain.length;
+    },
+    addLink(value) {
+        if (this.chain === undefined) {
+            this.chain = [];
+        }
+        this.chain.push(value);
+        return this;
+    },
+    removeLink(position) {
+        if (this.chain[position] === undefined) {
+            this.chain=[];
+            throw Error;
+        }
+        this.chain.splice(position - 1, 1);
+        return this;
+    },
+    reverseChain() {
+        if (this.chain === undefined) {
+            this.chain = [];
+        }
+        this.chain = this.chain.reverse();
+        return this;
+    },
+    finishChain() {
+        let result = '';
+        if (this.getLength() === 1) {
+            result= '( ' + this.chain[0] + ' )'
+        } else {
+            let first = '( ' + this.chain[0] + ' )~';
+            let last = '~( ' + this.chain[this.getLength() - 1] + ' )';
+
+            if (this.getLength() === 2) {
+                result= first + last;
+            } else {
+                result= first + this.chain.slice(1, -1).map(x => '~( ' + x + ' )~').join('') + last;
+            }
+
+        }
+        this.chain=[];
+        return result;
+    }
 };
 
 module.exports = chainMaker;
