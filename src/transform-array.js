cb57bad6ddeea8742b77230972d098f66e320630
sergyi.gonchar <sergyi.gonchar@gmail.com>
2020-09-29 22:48:57 +0300

    feat: resolve transform-array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,36 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+    let result = [];
+    if (arr instanceof Array) {
+        for (let i = 0; i < arr.length; i++) {
+            if (typeof arr[i] === "string") {
+                if (arr[i] === '--discard-next') {
+                    if(i!==arr.length-1){
+                      i++;
+                    }
+                } else if (arr[i] === '--discard-prev') {
+                  if(i!==0 && arr[i-2]!=='--discard-next'){
+                    result.pop();
+                  }
+                } else if (arr[i] === '--double-next') {
+                  if(i!==arr.length-1){
+                    result.push(arr[i+1]);
+                  }
+                } else if (arr[i] === '--double-prev') {
+                  if(i!==0 && arr[i-2]!=='--discard-next'){
+                    result.push(arr[i-1]);
+                  }
+                } else {
+                  result.push(arr[i]);
+                }
+            } else {
+              result.push(arr[i]);
+            }
+        }
+
+    } else {
+        throw Error;
+    }
+    return result;
 };
